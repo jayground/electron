5a84363c2c783f33c1c7d0bda067d4b83e2ab6de
jayground <jjoljjollee@gmail.com>
2017-10-27 16:42:14 +0900

    before finding coinone websocket

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const bittrex = require('node-bittrex-api');
+const rp = require('request-promise');
+
+const currencyOptions = {
+	method: 'POST',
+	uri: 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON/',
+	qs: {
+		authkey: '3MX72UclemPB219PmyNJINc8NR2oBGX3',
+		data: 'AP01'
+	},
+	json: true
+};
+const coinoneTicker = {
+	uri: 'https://api.coinone.co.kr/ticker/',
+	qs: {
+		currency: 'all'
+	},
+	json: true 
+}
+const coinoneOrderBook = {
+	uri: 'https://api.coinone.co.kr/orderbook/',
+	qs: {
+		currency: 'xrp'
+	},
+	json: true
+}
+
+let exchangeRate = 1100.0; //default 1100
+let btc_usd = 1.0;
+let btc_xrp = 1.0;
+let xrp = 1.0;
+let btc = 1.0;
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
@@ -22,7 +54,65 @@ app.on('ready', function() {
 	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
 	Menu.setApplicationMenu(mainMenu);
+
+	rp(currencyOptions)
+		.then((result) => {
+			result.forEach((info) => {
+				if(info.cur_unit == 'USD') {
+					exchangeRate = parseFloat(info.kftc_bkpr.replace(/,/g, ""));
+					mainWindow.webContents.send('send:exchangeRate', exchangeRate);
+					console.log('exchange rate: ', exchangeRate);
+					bittrexWebsocket();
+				} 
+			});
+		})
+		.catch((error) => {
+			console.log('error:getExchangeRate', error);
+		})
 });
+
+function bittrexWebsocket() {
+	bittrex.websockets.listen(function(data, client) {
+		if (data.M === 'updateSummaryState') {
+			console.log("----");
+			rp(coinoneTicker)
+				.then((result) => {
+					console.log("coinone hitted!");
+					xrp = parseFloat(result.xrp.last);
+					btc = parseFloat(result.btc.last);
+					console.log('coinone xrp : ', xrp);
+					console.log('coinone btc : ', btc);
+				})
+				.catch((err) => {
+					console.log("코인원 ticker 에러 발생");
+				});
+			rp(coinoneOrderBook)
+				.then((result) => {
+					console.log("orderbook : ", result.ask[0].price);
+				})
+				.catch((err) => {
+					console.log("orderbook error", err);
+				});
+			data.A.forEach(function(data_for) {
+				data_for.Deltas.forEach(function(marketsDelta) {
+					// console.log('Ticker Update for ' + marketsDelta.MarketName, marketsDelta);
+					if(marketsDelta.MarketName === 'BTC-XRP') {
+						// console.log('Ticker Update for ' + marketsDelta.MarketName, marketsDelta);
+						btc_xrp = parseFloat(marketsDelta.Last);
+						console.log('bittrex BTC_XRP: ', btc_xrp);
+						// console.log('bittrex XRT KRW type: ', typeof marketsDelta.Last);
+						mainWindow.webContents.send('send:price', marketsDelta);
+					}
+					if(marketsDelta.MarketName === 'USDT-BTC'){
+						btc_usd = parseFloat(marketsDelta.Last);
+						console.log('bittrex BTC USD: ', btc_usd);
+					}
+				});
+			});
+			console.log("bittrex BTC KRW: ", btc_usd * btc_xrp * exchangeRate);
+		}
+	});
+}
 
 function createAddWindow() {
 	addWindow = new BrowserWindow({
